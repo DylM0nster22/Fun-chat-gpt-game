9aec7be7991b0a023818bcb59c3afc7525f800d8
DylM0nster22 <120434344+DylM0nster22@users.noreply.github.com>
2023-06-29 13:30:39 -0700

    Setup basic game object

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
-
+var game = {
+    // The game's state (running, paused, ended)
+    state: 'rready',
+    
+    // The player's score
+    score: 0,
+    
+    // The player's lives
+    lives: 3,
+    
+    // The enemies that the player must avoid
+    enemies: [],
+    
+    // The items that the player must collect
+    items: [],
+    
+    // This function will be called when the game starts
+    start: function() {
+        // Todo
+    },
+    
+    // This function will be called when the game is updated
+    update: function() {
+        // Todo
+    },
+    
+    // This function will be called when the game is drawn
+    draw: function() {
+        // Todo
+    }
+};
